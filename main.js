92ed6be5dd7f6cabf4532acb2e576083c94ceb09
Shourya137 <75378252+Shourya137@users.noreply.github.com>
2021-09-01 19:37:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup(){
+    var canvas =  createCanvas(700,600);
+    canvas.parent('canvas');
+    
+    video = createCapture(VIDEO);
+    video.size(700, 600);
+    video.hide();
+    
+ 
+    }
+
+
+
+ 
